c8f4b9c4d8e06a14659648703a3477eec836b70a
Monica Li <monica.yc.li@gmail.com>
2019-02-06 11:41:12 -0500

    create our own neurodocker generator script

---
+++ docker/neurodocker.sh
@@ -0,0 +1,33 @@
+NEURODOCKER_IMAGE="kaczmarj/neurodocker:0.4.3"
+NIPYPE_BASE_IMAGE="nipype/nipype:base"
+PKG_MANAGER="apt"
+
+docker run --rm "$NEURODOCKER_IMAGE" generate docker \
+  --base "$NIPYPE_BASE_IMAGE" --pkg-manager "$PKG_MANAGER" \
+  --user neuro \
+  --miniconda create_env=neuro \
+              conda_install='python=3.6
+                             icu=58.1 libxml2 libxslt matplotlib mkl numpy paramiko
+                             pandas psutil scikit-learn scipy traits=4.6.0' \
+              pip_install="grabbit==0.1.2 https://github.com/INCF/pybids/tarball/0.6.5" \
+              activate=true \
+  --copy docker/files/run_builddocs.sh docker/files/run_examples.sh \
+         docker/files/run_pytests.sh nipype/external/fsl_imglob.py /usr/bin/ \
+  --copy . /src/nipype \
+  --user root \
+  --run 'chown -R neuro /src
+&& chmod +x /usr/bin/fsl_imglob.py /usr/bin/run_*.sh
+&& . /etc/fsl/fsl.sh
+&& ln -sf /usr/bin/fsl_imglob.py ${FSLDIR}/bin/imglob
+&& mkdir /work
+&& chown neuro /work' \
+  --user neuro \
+  --miniconda use_env=neuro \
+              pip_opts="-e" \
+              pip_install="/src/nipype[all]" \
+  --user root \
+  --install connectome-workbench \
+  -r 'cp /neurodocker/startup.sh /singularity' \
+  --user neuro \
+  --workdir /work
+
